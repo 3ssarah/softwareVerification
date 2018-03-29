2a950e8f34471fefdc2bac36db89ecfba1f6cc18
3ssarah <ssarha0731@gmail.com>
2018-03-29 23:54:23 +0900

    구구단

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,16 @@
 
 public class HelloWorld {
     public static void main(String[] args){
+
         System.out.println("Hello World!!");
+
+        System.out.println("----9X9 matrix------");
+        for(int i=1; i<=9; i++) {
+            for(int j=2; j<=9; j++) {
+                System.out.printf("%d * %d = %2d", j, i, (j*i));
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
     }
 }
